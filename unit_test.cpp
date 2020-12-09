0fc0883b30040062c88efa9b5a13c3625c9dace1
benjaminshu2001 <benjaminshu2001@gmail.com>
2020-12-08 22:23:11 -0800

    added visitor_test.hpp

--- unit_test.cpp
+++ unit_test.cpp
@@ -8,7 +8,8 @@
 #include "item_test.hpp"
 #include "inventory.h"
 #include "bubble_sort.h"
-#include "iterator_test.hpp"
+#include "visitor_test.hpp"
+
 int main(int argc, char**argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
